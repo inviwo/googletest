30ff6f13c9c2506331136ba7e8174e9425f99deb
Peter Steneteg <peter@steneteg.se>
2018-02-06 09:28:32 +0100

    temporary fix for issue #891

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -31,7 +31,8 @@
 #include <stdio.h>
 #include "gtest/gtest.h"
 
-GTEST_API_ int main(int argc, char **argv) {
+// disable GTEST_API_ see gtest issue #891
+/*GTEST_API_*/ int main(int argc, char **argv) {
   printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
